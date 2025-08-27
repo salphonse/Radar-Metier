affeb9edad9a9aa85c0c4d2704615e142a5b4ffd
oubeid38 <oubeid38@live.fr>
2025-08-27 18:38:18 +0200

    Add files via upload

---
+++ DossierFormulaire/DossierFormulaire/env/config.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_CONFIG = {
+  URL: 'https://bhckzdwrhhfaxbidmwpm.supabase.co',
+  KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoY2t6ZHdyaGhmYXhiaWRtd3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MTE1NzAsImV4cCI6MjA2NzE4NzU3MH0.qXODxUXjpfC9qTIHXBWcHXTLioJGXsnSrQqsJA1ugf4'
+};
